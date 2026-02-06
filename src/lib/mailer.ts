84733e0154757f0519d4e98a36a84fa5bae6265e
Bomberkill <ronaldkamgaing4@gmail.com>
2026-02-06 14:16:09 +0100

    fix: The Connection Pooling fix resolved the SSL crash

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -2,10 +2,21 @@ import nodemailer from 'nodemailer';
 
 export const transport = nodemailer.createTransport({
     host: process.env.SMTP_HOST3,
+    // port: 587,
+    // secure: false,
     port: 465,
     secure: true,
     auth: {
         user: process.env.SMTP_USER3,
         pass: process.env.SMTP_PASS3,
     },
+    debug: false,
+    logger: false,
+    pool: true,
+    maxConnections: 1,
+    // maxMessages: 100,
+    // tls: {
+    //     minVersion: 'TLSv1.2',
+    //     ciphers: 'DEFAULT:!SSLv3:!RC4:!DSS',
+    // }
 });
